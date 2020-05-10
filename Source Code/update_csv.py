83a7ef537abfd69caded4f67328dadb3be4bbcd3
Kyu Sang Han <kyuhan14@gmail.com>
2020-05-10 14:04:53 -0400

    gitignore created

---
+++ Source Code/update_csv.py
@@ -0,0 +1,19 @@
+# built-in libraries
+import os
+# external libraries
+import pandas as pd
+
+
+def update_csv(idx, fit, tag, setpath, **kwargs):
+	print('## update_csv.py')
+	datasheet = 'VAMPIRE datasheet ' + tag + '.csv'
+	if os.path.exists(os.path.join(setpath, datasheet)):
+		obj_ledger = pd.read_csv(os.path.join(setpath, datasheet))
+		obj_ledger['Shape mode'] = pd.Series(idx)
+		obj_ledger['Distance from cluster center'] = pd.Series(fit)
+		obj_ledger['Shape mode goodness of fit'] = pd.Series(kwargs['goodness'])
+		obj_ledger.to_csv(os.path.join(setpath, datasheet), index=False)
+	else:
+		d = {'Shape mode': pd.Series(idx), 'Distance from cluster center': pd.Series(fit)}
+		obj_ledger = pd.DataFrame(data=d)
+		obj_ledger.to_csv(os.path.join(setpath, datasheet), index=False, columns=["Shape mode", "Distance from cluster center"])
